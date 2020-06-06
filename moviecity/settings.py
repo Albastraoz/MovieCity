fad1e8998f7489fbcb7c0a8177a2a2997884c1e9
Robin Kaal <rkaal7@gmail.com>
2020-06-06 14:50:52 +0200

    Made env.py file to secure NEW secret key from public, also added home app to settings.py

--- moviecity/settings.py
+++ moviecity/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bowdm-1r@5k^9ek)ohath%zwd)@s8=2n1!jj9u&s5my#mpzxn$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
