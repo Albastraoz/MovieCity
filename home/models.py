e3936863ec30ebca5230a47b1bdf5958ef57148d
Robin Kaal <rkaal7@gmail.com>
2020-06-06 15:31:08 +0200

    Created model for movies and migrated

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    plot = models.TextField(max_length=500)
+    year = models.IntegerField(max_length=10)
+    rated = models.CharField(max_length=10)
+    released = models.CharField(max_length=50)
+    director = models.CharField(max_length=200)
+    poster = models.CharField(max_length=500)
+    imdbid = models.CharField(max_length=12)
+    imdbrating = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.title
